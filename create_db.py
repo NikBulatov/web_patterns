c43c363486243816ea27c39f78ff42bf3aa48916
Nikita Bulatov <nikitabulatov0699@gmail.com>
2023-09-28 08:51:21 +0300

    update scripts

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-with sqlite3.connect("db.sqlite3") as conn:
+with sqlite3.connect("db.sqlite") as conn:
     cur = conn.cursor()
     with open("create_db.sql", "r") as f:
         text = f.read()
